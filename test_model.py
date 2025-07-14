2d97e699b0a47282fdc4a343dbeb5c53c6e684d3
Arthur Maffre <maffrearthur@gmail.com>
2025-07-14 15:20:39 -0400

    Fixing import of idx2char

--- test_model.py
+++ test_model.py
@@ -253,7 +253,7 @@ def train_baseline(num_epochs=50, batch_size=128):
         if avg_loss < 0.5:
             break
 
-#train_baseline()
+train_baseline()
 
 torch.save(llm_model.state_dict(), "baseline_llm.pth")
 
